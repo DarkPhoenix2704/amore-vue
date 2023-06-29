e35e6b0fdc7b70f94fadaaa794919ecbe11806aa
DarkPhoenix2704 <anbarasun123@gmail.com>
2023-06-29 23:53:18 +0530

    feat: add google oauth

--- pages/auth.vue
+++ pages/auth.vue
@@ -10,9 +10,21 @@ useHead({
 });
 const supabase = useSupabaseAuthClient();
 
-const login = async () => {
+const loginGithub = async () => {
   const { error } = await supabase.auth.signInWithOAuth({
     provider: "github",
+  });
+
+  if (error) {
+    console.error(error);
+  } else {
+    navigateTo("/");
+  }
+};
+
+const loginGoogle = async () => {
+  const { error } = await supabase.auth.signInWithOAuth({
+    provider: "google",
   });
 
   if (error) {
@@ -29,11 +41,19 @@ const login = async () => {
     <p class="font-regular text-white">Confess Your Secrets Here</p>
     <button
       class="bg-[#262626] border-[#404040] border-[1px] flex flex-row items-center justify-center gap-1 text-white px-8 py-1 rounded-md"
-      @click="login"
+      @click="loginGithub"
     >
       <Icon name="uil:github" color="grey" class="text-2xl" />
 
       Sign In With Github
+    </button>
+    <button
+      class="bg-[#262626] border-[#404040] border-[1px] flex flex-row items-center justify-center gap-1 text-white px-8 py-1 rounded-md"
+      @click="loginGoogle"
+    >
+      <Icon name="uil:google" color="grey" class="text-2xl" />
+
+      Sign In With Google
     </button>
   </div>
 </template>
